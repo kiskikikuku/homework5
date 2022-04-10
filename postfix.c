a874b67457d52a7608f70160880a3302e4b83df5
TaeHyun Park <pthkuku99@gmail.com>
2022-04-10 21:06:54 +0900

    add postfix.c

--- postfix.c
+++ postfix.c
@@ -12,17 +12,16 @@ typedef enum{
     plus = 5,
     minus = 4,
     operand = 1
-} precedence;
+} precedence; // stack 내의 우선순위, 0이 가장 낮은 우선순위
 
-char infixExp[MAX_EXPRESSION_SIZE];
-char postfixExp[MAX_EXPRESSION_SIZE];
-char postfixStack[MAX_STACK_SIZE];
-int evalStack[MAX_STACK_SIZE];
+char infixExp[MAX_EXPRESSION_SIZE]; // infix 표현이 저장되는 배열
+char postfixExp[MAX_EXPRESSION_SIZE]; // postfix로 변경하여 저장할 배열
+char postfixStack[MAX_STACK_SIZE]; // postfix로 변환을 위한 스택
+int evalStack[MAX_STACK_SIZE]; // 계산용 스택
 
-int postfixStackTop = -1;
-int evalStackTop = -1;
-
-int evalresult = 0;
+int postfixStackTop = -1; // postfix 스택의 top
+int evalStackTop = -1; // 계산용 스택의 top
+int evalresult = 0; //계산 결과 저장
 
 void postfixPush(char x);
 char postfixPop();
@@ -39,7 +38,7 @@ void evaluation();
 
 int main(){
 
-    char command;
+    char command; //명령입력 문자
 
     do
     {
@@ -50,7 +49,7 @@ int main(){
       printf("----------------------------------------------------------------\n");
 
         printf("Command = ");
-        scanf(" %c", &command);
+        scanf(" %c", &command); // 명령 입력받기
 
         switch (command)
         {
@@ -81,40 +80,40 @@ int main(){
 }
 
 void postfixPush(char x){
-    postfixStack[++postfixStackTop] = x;
-}
+    postfixStack[++postfixStackTop] = x; // top을 하나 증가시키고 x삽입
+} //postfix 스택에 원소 1개 push
 
 char postfixPop(){
     char x;
-    if(postfixStackTop==-1)
+    if(postfixStackTop==-1) //비어있는 스택의 경우, 호출을 종료한다
         return '\0';
     else{
         x = postfixStack[postfixStackTop--];
-    }
+    } // x에 반환할 문자를 담고 top을 하나 감소시킨다
 
-    return x;
-}
+    return x; //pop하는 문자 반환
+} //postfix 스택에서 원소 1개 pop
 
 void evalPush(int x){
-    evalStack[++evalStackTop] = x;
-}
+    evalStack[++evalStackTop] = x; // top을 하나 증가시키고 x삽입
+} // 계산용 스택에 정수 push
 
 int evalPop(){
     int x;
     if (evalStackTop == -1)
-        return -1;
+        return -1; //비어있는 스택의 경우, 호출을 종료한다
     else{
         x = evalStack[evalStackTop--];
-    }
+    } // x에 반환할 정수를 담고 top을 하나 감소시킨다
     
-    return x;
-}
+    return x; //pop하는 숫자 반환
+} //계산용 스택에서 원소 1개 pop
 
 void getInfix(){
 
     printf("Type the expression >>> ");
     scanf("%s", infixExp);
-}
+} //infix표기 식을 문자열로 입력받는다
 
 precedence getToken(char symbol){
     switch (symbol)
@@ -127,20 +126,63 @@ precedence getToken(char symbol){
     case '/' : return divide;
     default: return operand;
     }
-}
+} // 입력받은 문자를 precedence로 변환하여줌
 
 precedence getPriority(char x){
     return getToken(x);
-}
+} 
 
 void charCat(char* c){
     if(postfixExp == '\0')
-        strcpy(postfixExp, c, 1);
+        strcpy(postfixExp, c);
     else
-        strcat(postfixExp, c, 1);
+        strcat(postfixExp, c);
 }
 
-void toPostfix(){} // 구현 대상 중위표기스택 -> 후위표기스택으로 변환
+void toPostfix(){
+    
+    char *exp = infixExp;
+    char x;
+
+    while (*exp != '\0')
+    {
+        if (getPriority(*exp) == operand) // 피연산자(숫자)의 경우
+        {
+            x = *exp;   //하나 저장해서
+            charCat(&x); //후위표기 Exp에 삽입한다.
+        }
+        else if (getPriority(*exp) == lparen) // 왼쪽괄호의 경우
+        {
+            
+            postfixPush(*exp); // 스택에 삽입한다.
+        }
+        else if (getPriority(*exp) == rparen){ // 오른쪽 괄호의 경우
+            while((x = postfixPop()) != '('){ // 왼쪽 괄호가 나올때까지 팝해서
+                charCat(&x); //후위표기 Exp에 삽입한다.
+            }
+        }
+        else //나머지 (+,-,*,/)
+        {
+            while(getPriority(postfixStack[postfixStackTop]) >= getPriority(*exp)){ //스택에 쌓인 연산자가 우선순위가 높은 경우
+                x = postfixPop(); //스택에서 1개 팝하여
+                charCat(&x); // 후위표기 Exp에 삽입
+            }
+            postfixPush(*exp); // 읽은 연산자를 후위표기 Exp에 삽입
+        }
+        
+        exp++; // 다음 문자 읽기
+    }
+
+    while (postfixStackTop != -1) //스택에 남은 연산자들을 Exp에 삽입
+    {
+        x = postfixPop;
+        charCat(&x);
+
+    }
+    
+
+         
+} // 구현 대상 중위표기스택 -> 후위표기스택으로 변환
 
 void debug(){
     printf("\n---DEBUG\n");
@@ -171,4 +213,42 @@ void reset(){
     evalresult = 0;
 }
 
-void evaluation(){} // 구현 대상 -> 계산
+void evaluation(){
+    int op1, op2;
+
+    char symbol;
+    evalStackTop = -1;
+    
+    for (int i = 0; i < strlen(postfixExp); i++)
+    {
+        symbol = postfixExp[i];
+        if (getToken(symbol)==operand) //숫자가 들어오면
+        {
+            evalPush(symbol -'0'); // 숫자를 계산 스택에 push
+        }
+        else{ // 연산자가 들어오면
+            op2 = evalPop(); 
+            op1 = evalPop(); //차례로 pop하여 변수에 넣어준다
+            switch (getToken(symbol))
+            {
+            case plus: 
+                evalPush(op1+op2);
+                break;
+            case minus:
+                evalPush(op1-op2);
+                break;
+            case times:
+                evalPush(op1*op2);
+                break;
+            case divide:
+                evalPush(op1/op2);
+                break; //연산 기호에 맞게 연산을 한 후 결과를 계산 스택에 push
+            default:
+                break;
+            }
+        }
+    }
+
+    evalresult = evalPop();
+    return;
+} // 구현 대상 -> 계산
